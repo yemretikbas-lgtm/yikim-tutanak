aacb5d60c61e1043832f9ce167e354c9ae367d06
yemretikbas-lgtm <yemre.tikbas@gmail.com>
2026-09-21 17:52:46 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='yikim-tutanak-v1.1.0-amb';
+const CACHE='yikim-tutanak-v1.2.0-accordion';
 const ASSETS=['./','./index.html','./jszip.min.js','./amb-data.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
