6681d79179c81f5b8a9840c8de57edcc2b9cbed8
yemretikbas-lgtm <yemre.tikbas@gmail.com>
2026-09-21 17:27:39 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='yikim-tutanak-v1.0.0';
-const ASSETS=['./','./index.html','./jszip.min.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+const CACHE='yikim-tutanak-v1.1.0-amb';
+const ASSETS=['./','./index.html','./jszip.min.js','./amb-data.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const cp=res.clone();caches.open(CACHE).then(c=>c.put(e.request,cp));return res}).catch(()=>caches.match('./index.html'))))});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const cp=res.clone();caches.open(CACHE).then(c=>c.put(e.request,cp)).catch(()=>{});return res}).catch(()=>{if(e.request.mode==='navigate')return caches.match('./index.html');return Response.error()})))});
